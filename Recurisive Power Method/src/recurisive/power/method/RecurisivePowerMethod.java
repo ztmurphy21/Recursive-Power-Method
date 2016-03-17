29b6e3bf2f4b5ef3e867e22a766f4cf130749ae8
Zachary Murphy <murphyz@my.wccc.edu>
2016-03-17 19:43:32 -0400

    comments
    comments added.

--- Recurisive Power Method/src/recurisive/power/method/RecurisivePowerMethod.java
+++ Recurisive Power Method/src/recurisive/power/method/RecurisivePowerMethod.java
@@ -1,33 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package recurisive.power.method;
 
 import java.util.Scanner;
 
 /**
  *
- * @author zacha
+ * @author Zach Murphy
  */
 public class RecurisivePowerMethod {
 
-    /**
-     * @param args the command line arguments
-     */
+
+    
+    //main method 
     public static void main(String[] args) {
+        //scanner object
         Scanner a= new Scanner(System.in);
+        //declarations 
         double x, y;
+        //getting some variables from user and setting them with keyboard scanner
         System.out.println("Enter a number to be raised to a power.");
         x = a.nextDouble();
         System.out.println("Enter the exponent to rase the base to.");
         y = a.nextDouble();
+        
+        //output from recursion method
         System.out.println(x + " raised to the power of " + y + " is " + pow(x ,y));
     }
     
-    
+    //recursive method
     public static double pow (double x, double y){
+        
+        //setting calculations of power.
         if ( y  > 0){
             return x * pow(x, y-1);
         }else{
